cce78e21e45bbaa7044335a98d2d3c29bed678eb
Jakub Dolata <jdolata5@gmail.com>
2021-08-22 13:10:50 +0200

    add styles config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,50 @@
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Ruszkowski",
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    // {
-    //   resolve: "gatsby-plugin-google-analytics",
-    //   options: {
-    //     trackingId: "",
-    //   },
-    // },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
+    siteMetadata: {
+        siteUrl: "https://ruszkowski.biz",
+        title: "Ruszkowski",
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+    plugins: [
+        {
+            resolve: "gatsby-plugin-sass",
+            options: {
+                postCssPlugins: [
+                    autoprefixer({
+                        browsers: ['last 2 versions', 'not ie 10'],
+                        grid: true,
+                    })
+                ],
+                useResolveUrlLoader: {
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+            },
+        }, "gatsby-plugin-image",
+        // {
+        //   resolve: "gatsby-plugin-google-analytics",
+        //   options: {
+        //     trackingId: "",
+        //   },
+        // },
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
