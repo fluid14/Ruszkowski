843f47f4545f409b8ad7f34c3191b566856acf6b
Jakub Dolata <jdolata5@gmail.com>
2021-08-29 20:52:13 +0200

    add blog query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,18 +1,76 @@
 import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+import PropTypes from 'prop-types';
 import Theme from '../theme/Theme';
 import Header from '../components/shared/Header/Header';
 import headerImg from '../../static/images/blog/header.png';
+import * as styles from './Blog.module.sass';
 
-const Blog = () => (
-  <>
-    <Theme>
-      {/* <Header */}
-      {/*  title={['Realizacje', <br />, <span>Indywidualne</span>]} */}
-      {/*  bgc={headerImg} */}
-      {/* /> */}
-      <Header title="Blog" bgc={headerImg} />
-    </Theme>
-  </>
+const BlogComponent = ({ data }) => {
+  const {
+    blog_banner_title: { html: bannerTitle },
+    blog_description_title: { html: blogDescriptionTitle },
+    blog_description: { html: blogDescription },
+  } = data.prismicPage.data;
+
+  return (
+    <>
+      <Theme>
+        {/* <Header */}
+        {/*  title={['Realizacje', <br />, <span>Indywidualne</span>]} */}
+        {/*  bgc={headerImg} */}
+        {/* /> */}
+        <Header title="Blog" bgc={headerImg} />
+        <main className={(styles.blogWrap, 'wrap')}>
+          <h1>
+            NASZE <span>TREŚCI</span>
+          </h1>
+          {/* <ReactMarkdown children={blogDescription} /> */}
+        </main>
+      </Theme>
+    </>
+  );
+};
+
+const Blog = (props) => (
+  <StaticQuery
+    query={graphql`
+      query HeaderQuery {
+        prismicPage {
+          data {
+            blog_description_title {
+              html
+            }
+            blog_description {
+              html
+            }
+            blog_banner_title {
+              html
+            }
+          }
+        }
+      }
+    `}
+    render={(data) => <BlogComponent data={data} {...props} />}
+  />
 );
+
+BlogComponent.propTypes = {
+  data: PropTypes.shape({
+    prismicPage: PropTypes.shape({
+      data: PropTypes.shape({
+        blog_description_title: PropTypes.shape({
+          html: PropTypes.string.isRequired,
+        }).isRequired,
+        blog_description: PropTypes.shape({
+          html: PropTypes.string.isRequired,
+        }).isRequired,
+        blog_banner_title: PropTypes.shape({
+          html: PropTypes.string.isRequired,
+        }).isRequired,
+      }).isRequired,
+    }),
+  }).isRequired,
+};
 
 export default Blog;
