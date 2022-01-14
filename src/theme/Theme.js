f651526972be2e7cc51685545f08b796dcc68608
Jakub Dolata <jdolata5@gmail.com>
2022-01-14 20:11:59 +0100

    add page transition

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
+import GatsbyImage from 'gatsby-image';
 import Navbar from '../components/layout/Navbar/Navbar';
 import PageOrnament from '../components/layout/PageOrnament/PageOrnament';
 import 'normalize.css';
 import Footer from '../components/layout/Footer/Footer';
 import { motion } from 'framer-motion';
+import { translate } from '../utils/translate';
 
 const blackBox = {
   initial: {
     bottom: 0,
     height: '100vh',
+    width: '100vw',
   },
   animate: {
-    height: 0,
+    height: '100vh',
+    width: 0,
   },
   exit: {
     bottom: 0,
     height: '100vh',
+    width: '100vw',
+  },
+};
+
+const logoVar = {
+  initial: {
+    opacity: 1,
+  },
+  animate: {
+    opacity: 0,
+  },
+  exit: {
+    opacity: 1,
   },
 };
 
@@ -25,6 +42,7 @@ const ThemeComponent = ({
   children,
   lang,
   data: {
+    allPrismicSettings: { nodes: settings },
     prismicSettings: {
       data: { logo, footer_links: footerLinks },
     },
@@ -41,7 +59,26 @@ const ThemeComponent = ({
         duration: 1.5,
         ease: [0.87, 0, 0.13, 1],
       }}
-    />
+    >
+      <motion.div
+        className="preloadLogoWrap"
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={logoVar}
+        transition={{
+          duration: 0.2,
+          delay: 0.5,
+          ease: 'easeInOut',
+        }}
+      >
+        <GatsbyImage
+          className="preloadLogo"
+          alt={translate(lang, settings).logo.alt}
+          fluid={translate(lang, settings).logo.fluid}
+        />
+      </motion.div>
+    </motion.div>
     <main className="pageWrap">
       <Navbar lang={lang} />
       <PageOrnament />
@@ -55,12 +92,25 @@ const Theme = (props) => (
   <StaticQuery
     query={graphql`
       query FooterQuery {
+        allPrismicSettings {
+          nodes {
+            lang
+            data {
+              logo {
+                alt
+                fluid {
+                  ...GatsbyImgixFluid
+                }
+              }
+            }
+          }
+        }
         prismicSettings {
           data {
             logo {
               alt
               fluid {
-                ...GatsbyImgixFluid_noBase64
+                ...GatsbyImgixFluid
               }
             }
             footer_links {
@@ -84,6 +134,7 @@ ThemeComponent.propTypes = {
   ]).isRequired,
   lang: PropTypes.string.isRequired,
   data: PropTypes.shape({
+    allPrismicSettings: PropTypes.shape({ nodes: PropTypes.shape() }),
     prismicSettings: PropTypes.shape({
       data: PropTypes.shape({
         logo: PropTypes.shape.isRequired,
