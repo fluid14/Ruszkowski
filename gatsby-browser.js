48b0dbaa3a6c7c355cb2761780e6ffb0ea6c8dbc
Jakub Dolata <jdolata5@gmail.com>
2022-01-17 20:46:46 +0100

    fix preloader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,8 @@ import { AnimatePresence } from 'framer-motion';
 export function wrapPageElement({ element }) {
   return <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>;
 }
+
+export function shouldUpdateScroll() {
+  setTimeout(() => window.scrollTo(0, 0), 1400);
+  return false;
+}
