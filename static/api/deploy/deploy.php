e4a4c01dd94d412f8afe1b76e4b58f504d1465f3
Jakub Dolata <jdolata5@gmail.com>
2022-04-08 18:33:45 +0200

    add deploy files

---
+++ static/api/deploy/deploy.php
@@ -0,0 +1,11 @@
+  <?php
+                    require_once('./httpful.phar');
+
+                    $response = \Httpful\Request::post('https://api.github.com/repos/fluid14/Ruszkowski/dispatches')
+                                        ->sendsJson()
+                                        ->body(['event_type' => 'deploy'], \Httpful\Mime::JSON)
+                                        ->addHeader('Authorization', 'token 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')
+                                        ->addHeader('Accept', 'application/vnd.github.everest-preview+json')
+                                        ->send();
+
+                    var_dump($response->body);
